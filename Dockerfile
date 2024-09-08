535b22add98764d79bade4f57804ac114b38ea71
“Ollie <ollie@beenhamow.co.uk>
2024-09-08 20:57:54 +0100

    Updated the creation of the samba user and the logging

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,25 @@
+# Use the latest version of Ubuntu as the base image
 FROM ubuntu:latest
 
+# Update the package list and install Samba and gettext-base
 RUN apt-get update && \
-    apt-get install -y samba && \
+    apt-get install -y samba gettext-base && \
     rm -rf /var/lib/apt/lists/*
 
+# Copy the Samba configuration file to the appropriate directory
 COPY ./config/smb.conf /etc/samba/smb.conf
 
+# Expose the necessary ports for Samba
 EXPOSE 137/udp 138/udp 139 445
 
+# Create the directory for the Samba share, set ownership, and permissions
 RUN mkdir -p /srv/samba/share && \
     chown nobody:nogroup /srv/samba/share && \
     chmod -R 0777 /srv/samba/share
 
+# Copy the startup script to the appropriate directory and make it executable
 COPY ./config/start.sh /usr/local/bin/start-samba.sh
 RUN chmod +x /usr/local/bin/start-samba.sh
 
+# Set the startup script as the container's entry point
 CMD ["/usr/local/bin/start-samba.sh"]
--- config/start.sh
+++ config/start.sh
@@ -2,20 +2,53 @@
 
 # Check if required environment variables are set
 if [ -z "$SAMBA_USER" ] || [ -z "$SAMBA_PASSWORD" ] || [ -z "$SAMBA_SHARE" ]; then
-    echo "Error: SAMBA_USER, SAMBA_PASSWORD, and SAMBA_SHARE environment variables must be set."
+    echo "Error: "
+    if [ -z "$SAMBA_USER" ]; then
+        echo "  SAMBA_USER environment variable must be set."
+    fi
+    if [ -z "$SAMBA_PASSWORD" ]; then
+        echo "  SAMBA_PASSWORD environment variable must be set."
+    fi
+    if [ -z "$SAMBA_SHARE" ]; then
+        echo "  SAMBA_SHARE environment variable must be set."
+    fi
     exit 1
 fi
 
 # Substitute environment variables in smb.conf
 envsubst < /etc/samba/smb.conf > /etc/samba/smb.conf.tmp && mv /etc/samba/smb.conf.tmp /etc/samba/smb.conf
 
-# Add Samba user
-if [ -n "$SAMBA_USER" ] && [ -n "$SAMBA_PASSWORD" ]; then
-    (echo "$SAMBA_PASSWORD"; echo "$SAMBA_PASSWORD") | smbpasswd -s -a "$SAMBA_USER"
+# Check if Samba user exists
+if pdbedit -L | grep -q "^$SAMBA_USER:"; then
+    echo "Samba user $SAMBA_USER already exists."
+else
+    # Create user
+    adduser --no-create-home --disabled-password --disabled-login --gecos "" $SAMBA_USER
+
+    # Add Samba user
+    if [ -n "$SAMBA_USER" ] && [ -n "$SAMBA_PASSWORD" ]; then
+        echo "Adding Samba user: $SAMBA_USER"
+        (echo "$SAMBA_PASSWORD"; echo "$SAMBA_PASSWORD") | smbpasswd -s -a "$SAMBA_USER"
+        
+        if [ $? -eq 0 ]; then
+            echo "User $SAMBA_USER created successfully."
+        fi
+    fi
 fi
 
+# Log the smb.conf file
+printf "\n-------------- smb.conf --------------\n\n"
+cat /etc/samba/smb.conf
+
+# Log Configuration
+printf "\n-------------- CONFIGURATION --------------\n"
+printf "\n\033[0;32mSAMBA USER: \033[1;37m$SAMBA_USER"
+printf "\n\033[0;32mSAMBA PASSWORD: \033[1;37m$SAMBA_PASSWORD"
+printf "\n\033[0;32mSAMBA SHARE: \033[1;37m$SAMBA_SHARE\n\n"
+echo "-------------------------------------------"
+
 # Start the smbd service (Samba daemon)
-/usr/sbin/smbd --foreground --no-process-group
+/usr/sbin/smbd --no-process-group
 
 # Keep the container running
 tail -f /dev/null
